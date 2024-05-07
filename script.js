a132818f4dbf8383e9699a3d7f0832879797b8cf
Anisha-khadka <anishakhadka434@gmail.com>
2024-05-07 16:26:39 +0545

    Second commit

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
-let listBox= document.getElementById("list-container");
-let inputBox =document.getElementById("input-box");
-
-
-
+let listBox = document.getElementById("list-container");
+let inputBox = document.getElementById("input-box");
 
 function addTodo() {
-    if(inputBox.value===""){
-        alert("Please write your todo");
-    }
-    else{
-       let todo= document.createElement('li');
-        todo.textContent=inputBox.value
-          listBox.appendChild(todo);
+  if (inputBox.value === "") {
+    alert("Please write your todo");
+  } else {
+    let todo = document.createElement("li");
+    todo.textContent = inputBox.value;
+    listBox.appendChild(todo);
 
-        let span= document.createElement('span');
-        span.textContent='\u00d7';
-        todo.appendChild(span)
+    let closeSymbol = document.createElement("span");
+    closeSymbol.textContent = "\u00d7";
+    closeSymbol.classList.add("close-symbol");
+    todo.appendChild(closeSymbol);
 
-  
 
-    }
-    inputBox.value=" "; 
-    saveData();
+    let anotherSymbol = document.createElement("span");
+    anotherSymbol.textContent = "\u270E"; 
+    anotherSymbol.classList.add("another-symbol");
+
+    todo.appendChild(anotherSymbol);
+  }
+  inputBox.value = " ";
+  saveData();
 }
-listBox.addEventListener('click',(e)=>{
-    if(e.target.tagName=="LI"){
-        e.target.classList.toggle('checked')
-        saveData();
-    }
-    else if(e.target.tagName=="SPAN"){
+listBox.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName == "SPAN") {
+    if(e.target.classList.contains("close-symbol")){
         e.target.parentElement.remove();
         saveData();
     }
-})
- function saveData(){
-   localStorage.setItem("tasks",listBox.innerHTML)
-}
- function showData(){
-    listBox.innerHTML=localStorage.getItem('tasks')
     
+    if (e.target.classList.contains("another-symbol")) {
+        if(!e.target.parentElement.classList.contains("checked")){
+        inputBox.value = e.target.parentElement.textContent.replace("\u00d7","").replace("\u270E","").trim();
+        e.target.parentElement.remove();
+        saveData();
+        }
+      }
+    
+   
+  }
+});
+function saveData() {
+  localStorage.setItem("tasks", listBox.innerHTML);
 }
-function clearall(){
-    localStorage.clear();
-
+function showData() {
+  listBox.innerHTML = localStorage.getItem("tasks");
 }
-showData()
+function clearall() {
+  localStorage.clear();
+}
+showData();
